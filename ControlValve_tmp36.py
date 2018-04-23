f7c347f7f23a257bddbdc5e7f467f6a7d175174f
Kohei Onda <onda@minelab.jp>
2018-04-23 19:00:08 +0900

    support AM2302

---
+++ ControlValve_tmp36.py
@@ -0,0 +1,123 @@
+'''
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
+import tmp36
+import RPi.GPIO as GPIO
+
+PIN = 7
+def valveOpen(client, userdata, message):
+
+    if not GPIO.input(PIN):
+        print("Open valve:{}".format(message.payload))
+        GPIO.output(PIN,True)
+    
+def valveClose(client, userdata, message):
+
+    if GPIO.input(PIN):
+        print("Close valve:{}".format(message.payload))
+        GPIO.output(PIN,False)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN,GPIO.OUT)
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                    help="Targeted client id")
+parser.add_argument("-t", "--sendtopic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic for sending temperature data")
+parser.add_argument("-ot", "--opentopic", action="store", dest="opent", default="sdk/test/Python", help="Targeted topic for opening valve")
+parser.add_argument("-ct", "--closetopic", action="store", dest="closet", default="sdk/test/Python", help="Targeted topic for closing valve")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+tempt = args.topic
+opent = args.opent
+closet = args.closet
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, 443)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe(opent, 1, valveOpen)
+myAWSIoTMQTTClient.subscribe(closet, 1, valveClose)
+
+time.sleep(2)
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+try:
+    while True:
+        time.sleep(5)
+        t = tmp36.tmp36()
+        #myAWSIoTMQTTClient.publish(topic, "New Message " + str(loopCount), 1)
+        data = {
+            "temperature" : t.getTemp(0)
+        }
+        myAWSIoTMQTTClient.publish(tempt, json.dumps(data), 1)
+        print("send:{}".format(data))
+        loopCount += 1
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    
